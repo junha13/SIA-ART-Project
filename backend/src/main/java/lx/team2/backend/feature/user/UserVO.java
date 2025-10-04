acce7f93950cd7a48b20c5e528e15af12f27adee
jiseoheo <jiseo.heo@gmail.com>
2025-10-04 10:37:48 +0900

    DB에 맞게 userVO 수정

--- backend/src/main/java/lx/team2/backend/feature/user/UserVO.java
+++ backend/src/main/java/lx/team2/backend/feature/user/UserVO.java
@@ -12,77 +12,22 @@ public class UserVO {
 	 * 테이블의 모든 정보를 포함함
 	 */
 	
-	private int userId;
+	private int userNumber; // user_number
 	private String id;
-    private String username;
     private String password;
-    private String role; // "USER" or "ARTIST"
-	private String penName;
+    private String userName; // user_name
+	private String activityName; // activity_name
 	private String mobile;
 	private String email;
-	private LocalDate birth;
+	private LocalDate birthday;
+    private String roleNumber; // role_number
 	private int locationId;
+    private double locationLon; // location 경도
+	private double locationLat; // location 위도
+	private String[] interestsList;
+
+	public String[] getInterestsList() {
+		return interestsList;
+	}
 	
-	public int getUserId() {
-		return userId;
-	}
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getRole() {
-		return role;
-	}
-	public void setRole(String role) {
-		this.role = role;
-	}
-	public String getPenName() {
-		return penName;
-	}
-	public void setPenName(String penName) {
-		this.penName = penName;
-	}
-	public String getMobile() {
-		return mobile;
-	}
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public LocalDate getBirth() {
-		return birth;
-	}
-	public void setBirth(LocalDate birth) {
-		this.birth = birth;
-	}
-	public int getLocationId() {
-		return locationId;
-	}
-	public void setLocationId(int locationId) {
-		this.locationId = locationId;
-	}
-    
-    
 }
